55f47f875d00d1186a202b6c5ce62cd4af924701
Marise Hayashi <nobody.one@nowhere.space>
2019-06-22 21:49:09 +0300

    Apply evasions

--- modules/pool.js
+++ modules/pool.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra").use(require("puppeteer-extra-plugin-stealth")());
 const genericPool = require("generic-pool");
 
 const initPuppeteerPool = (
